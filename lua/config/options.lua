1dbaf9b1f12ca84d9ca6267200befaef84c6dbda
Yousef <190332698+yousefbase@users.noreply.github.com>
2025-12-01 14:12:26 +0000

    base

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,7 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 
 
-vim.opt.tabstop = 4         -- Number of spaces a <Tab> character counts for.
-vim.opt.shiftwidth = 4      -- Number of spaces to use for each step of (auto)indent.
-vim.opt.expandtab = true    -- Convert tabs to spaces. Set to false to use actual tab characters.
-vim.opt.softtabstop = 4     -- Number of spaces that a <Tab> counts for when editing.Vh
+vim.opt.tabstop = 4     
+vim.opt.shiftwidth = 4      
+vim.opt.expandtab = true    
+vim.opt.softtabstop = 4     
